45b049f95319c3bd47e64e2d1cf46e65d42acaa0
Shreya Patil <patilshreya376@gmail.com>
2025-11-12 13:43:11 +0530

    Implemented all the 3 dependency injections

--- StudentManagment/src/main/java/com/example/demo/controller/StudentController.java
+++ StudentManagment/src/main/java/com/example/demo/controller/StudentController.java
@@ -1,23 +1,46 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.service.StudentService;
+import com.example.demo.service.CollegeServiceImpl;
+import com.example.demo.service.PhdServiceImpl;
+import com.example.demo.service.SchoolServiceImpl;
 
 @RestController
 public class StudentController {
 
-	@Autowired
-	ApplicationContext factory;
+	private SchoolServiceImpl schoolServiceImpl;
+	private CollegeServiceImpl collegeServieImpl;
+	
+	//field injection
+	@Autowired(required = false)
+	PhdServiceImpl phdServiceImpl;
+	
+	@Autowired(required = false) //setter injection
+	public void setStudentService(@Qualifier("schoolServiceImpl") SchoolServiceImpl schoolServiceImpl) {
+		this.schoolServiceImpl = schoolServiceImpl;
+	}
+
+	//constructor injection
+	 StudentController(@Qualifier("collegeServiceImpl") CollegeServiceImpl collegeServieImpl) {
+		this.collegeServieImpl = collegeServieImpl; 
+	}
 	
 	@GetMapping("fees/{type}")
 	public String getFees(@PathVariable String type) {
 		
-		StudentService ss = (StudentService)factory.getBean(type);
-		return ss.getFees();
+		if(type.equals("school"))
+			return schoolServiceImpl.getFees();
+		
+		if(type.equals("college"))
+			return collegeServieImpl.getFees();
+		
+		if(type.equals("phd"))
+			return phdServiceImpl.getFees();
+					
+		return null;
 	}
 }
---
+++ StudentManagment/src/main/java/com/example/demo/service/PhdServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhdServiceImpl implements StudentService {
+
+	@Override
+	public String getFees() {
+		// TODO Auto-generated method stub
+		return "PHD Fees is 20L";
+	}
+
+}
