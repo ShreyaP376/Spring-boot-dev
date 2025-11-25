f5fec0548c0f93557432cc9b049fc04c93d79835
Shreya Patil <patilshreya376@gmail.com>
2025-11-25 16:18:54 +0530

    Removed duplicates from HashSet

--- Collections/src/Collections.java
+++ Collections/src/Collections.java
@@ -1,41 +1,40 @@
+import java.util.HashSet;
+import java.util.Objects;
 
 public class Collections {
 	
 	public static void main(String arg[]) {
+		Employee e1 = new Employee(123, "John", "pune");
+		Employee e2 = new Employee(123, "John", "pune");
+		Employee e3 = new Employee(124, "BAOB", "mumbai");
 		
-		Employee e1 = new Employee("John","pune");
-		Employee e2 = new Employee("bob", "pune");
-		System.out.println(e1.toString());
+		HashSet s = new HashSet();
+		s.add(e1);
+		s.add(e2);
+		s.add(e3);
+		
+		System.out.println(s.toString());
 		System.out.println(e1.equals(e2));
-		
 	}
 	
 }
 
 class Employee{
+	private int id;
 	private String name;
 	private String address;
-	
-	public Employee(String name, String address) {
-		// TODO Auto-generated constructor stub
+	public Employee(int id, String name, String address) {
+		super();
+		this.id = id;
 		this.name = name;
 		this.address = address;
 	}
-
-	@Override
-	public String toString() {
-		return "Employee [name=" + name + ", address=" + address + "]";
+	public int getId() {
+		return id;
 	}
-	
-	public boolean equals(Object obj) {
-		Employee e2 = (Employee) obj;
-		
-		if(e2.getName() == name && e2.getAddress() == address) {
-			return true;
-		}
-		return false;
+	public void setId(int id) {
+		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
@@ -48,5 +47,32 @@ class Employee{
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", address=" + address + "]";
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) {
+			return true;
+		}
+		if(o instanceof Employee) {
+			Employee e = (Employee) o;
+			
+		if(id == e.getId()) {
+			return true;
+		}
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id,name, address);
+	}
+	
+	
 	
 }
